2c3998f4d92c52c27eac91847b2c5ef147281b6e
nirmala <nirmala@surf.net.np>
2018-12-28 13:30:26 +0100

    add child component to main app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './img-list/img-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ImageListComponent]
 })
 export class AppModule { }
