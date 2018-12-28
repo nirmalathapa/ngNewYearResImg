090c9b93400ea4c1f667d39a3ba63436868eb4c5
nirmala <nirmala@surf.net.np>
2018-12-28 20:08:00 +0100

    update title name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'List of New year Resolutions';
+  title = '2019 New year Resolutions';
 }
