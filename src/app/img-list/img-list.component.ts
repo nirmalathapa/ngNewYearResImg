4928ce8f83b0b7eccb264b5354dce55e18b7e78b
nirmala <nirmala@surf.net.np>
2018-12-28 20:10:52 +0100

    get images from model rather than directly writing in html src

--- src/app/img-list/img-list.component.ts
+++ src/app/img-list/img-list.component.ts
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ImageListComponent implements OnInit {
-  title = 'List of Images';
-  constructor() { }
+  images: string[];
+  constructor() {
+    this.images = [
+      'assets/1.jpg',
+      'assets/2.jpg',
+      'assets/3.jpg',
+      'assets/4.jpg',
+      'assets/5.jpg',
+      'assets/6.jpg',
+      'assets/7.jpg',
+      'assets/9.jpg',
+      'assets/10.jpg',
+      'assets/11.jpg',
+      'assets/12.jpg',
+    ];
+  }
   ngOnInit() {
   }
 }
